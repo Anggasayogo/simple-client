9051e4db95e7ae669b8faccf5256186ae7ddbf0d
Anggasayogo <anggasayogosm@gmail.com>
2022-01-16 13:46:18 +0700

    "feat: client"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { io } from "socket.io-client";
+import axios from 'axios'
 
 function App() {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    const getOrders = async () => {
+      const response = await axios.get('http://localhost:5000/orders')
+      const ordersData = response.data;
+      setOrders(ordersData)
+    } 
+
+    getOrders()
+  }, [])
+
+  useEffect(() => {
+    const socket = io('ws://localhost:5000')
+
+    socket.on('connnection', () => {
+      console.log('connected to server');
+    })
+
+    socket.on('order-added', (newOrders) => {
+      setOrders(newOrders)
+    })
+
+    socket.on('message', (message) => {
+      console.log(message);
+    })
+
+    socket.on('disconnect', () => {
+      console.log('Socket disconnecting');
+    })
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
